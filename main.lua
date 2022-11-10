a95fa719cea8ae21cd6d6809fd37ef92bc98f922
Matheus <mfabryny@minsait.com>
2022-11-10 19:03:36 -0300

    Initital architecture

---
+++ main.lua
@@ -0,0 +1,38 @@
+require 'src/Dependencies'
+
+function love.load()
+    math.randomseed(os.time())
+    love.window.setTitle('Legend of Zelda')
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        vsync = true,
+        resizable = true
+    })
+
+    love.graphics.setFont(gFonts['small'])
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+    push:start()
+    
+    push:finish()
+end
---
+++ src/Dependencies.lua
@@ -0,0 +1,12 @@
+Class = require 'lib/class'
+push = require 'lib/push'
+
+
+require 'src/constants'
+
+
+gFonts = {
+    ['small'] = love.graphics.newFont('fonts/font.ttf', 8),
+    ['medium'] = love.graphics.newFont('fonts/font.ttf', 16),
+    ['large'] = love.graphics.newFont('fonts/font.ttf', 32),
+}
